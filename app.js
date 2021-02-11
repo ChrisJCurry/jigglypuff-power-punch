1a13953f4a3207211c9842884bd76e687d6c9341
ChrisJCurry <cjcurrybusiness@gmail.com>
2021-02-11 14:21:01 -0700

    Added functions and set item objects

--- app.js
+++ app.js
@@ -2,5 +2,81 @@ let jigglyHP = 100
 let userStamina = 0
 let userKicks = 0
 let jiggliesPuffed = 0
+let evolved = false
 
+let normalAttacks = {
+    punch: {
+        damage: 20,
+        staminaGained: 20
+    },
+    inflate: {
+        damage: 30,
+        staminaGained: 40
+    },
+    rest: {
+        damage: 0,
+        staminaGained: 100
+    }
+}
+
+let fairyAttacks = {
+    sing: {
+        damage: 10,
+        staminaGained: -10
+    },
+    dazzlingGleam: {
+        damage: 15,
+        staminaGained: -20
+    },
+    evolve: {
+        damage: 0,
+        staminaGained: -100
+    }
+}
+
+function getAttack(attack) {
+    console.log(attack)
+}
+
+function setHP(damage) {
+    let healthText = document.getElementById("health-bar")
+    jigglyHP -= damage
+    if(jigglyHP <= 0) {
+        //jiggly is knocked unconscious
+        jiggliesPuffed++
+        jigglyHP = 0
+    }
+
+    healthText.innerText = `${jigglyHP}`
+}
+
+function setImg() {
+
+}
+
+function setEvolved(isEvolved) {
+    if(isEvolved) {
+        evolved = true
+    } else { 
+        evolved = false
+    }
+}
+
+function setStamina(staminaGiven) {
+    let staminaText = document.getElementById("stamina-bar")
+    userStamina += staminaGiven
+    if(userStamina <= 0) {
+        userStamina = 0
+    } else if (userStamina >= 100) {
+        userStamina = 100
+    }
+
+    staminaText.innerText = `${userStamina}`
+}
+
+function reset() {
+    let healthText = document.getElementById("health-bar")
+    let staminaText = document.getElementById("stamina-bar")
+    setEvolved(false)
+}
 
